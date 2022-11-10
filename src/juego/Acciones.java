f7bbafdb57e3ad94a0c2d1eba1bef2077c48d96e
Franco <Francoernestocollins@gmail.com>
2022-11-10 19:09:24 +0100

    comenzamos a iniciar la partida en clase Enfrentamiento

--- src/juego/Acciones.java
+++ src/juego/Acciones.java
@@ -1,22 +1,29 @@
 package juego;
 
 public class Acciones {
-    public static int ENVIAR_SELECCION        = 1; // Piedra papel o tijera
-    public static int ENVIAR_SELECCION_RIVAL  = 2;
-    public static int ENVIAR_NOMBRE           = 3;
-    public static int FINAL_DE_RONDA          = 4;
-    public static int FINAL_DE_ENFRENTAMIENTO = 5;
-    public static int FINAL_DE_TORNEO         = 6;
-
-    public static int PIEDRA                  = 100;
-    public static int PAPEL                   = 101;
-    public static int TIJERA                  = 102;
+    public static int ENVIAR_SELECCION          = 1; // Piedra papel o tijera
+    public static int ENVIAR_SELECCION_RIVAL    = 2;
+    public static int ENVIAR_NOMBRE             = 3;
+    public static int FINAL_DE_RONDA            = 4;
+    public static int FINAL_DE_ENFRENTAMIENTO   = 5;
+    public static int FINAL_DE_TORNEO           = 6;
 
 
-    public static int ERROR                   = -1;
-    public static int SELECCION_INCORRECTA    = -2;
 
-    public static int SEPARADOR               = '|';
+
+
+    public static int PIEDRA                    = 100;
+    public static int PAPEL                     = 101;
+    public static int TIJERA                    = 102;
+
+    public static int GANADOR_DE_RONDA          = 200;
+    public static int GANADOR_DE_TORNEO         = 201;
+    public static int GANADOR_DE_ENFRENTAMIENTO = 202;
+
+    public static int ERROR                     = -1;
+    public static int SELECCION_INCORRECTA      = -2;
+
+    public static int SEPARADOR                 = '|';
 
 }
 
