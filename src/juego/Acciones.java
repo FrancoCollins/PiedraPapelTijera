798dd8273f597cf386d2113e8e6866150efb763b
Franco <Francoernestocollins@gmail.com>
2022-11-10 20:09:15 +0100

    Changes by clancy

--- src/juego/Acciones.java
+++ src/juego/Acciones.java
@@ -1,24 +1,24 @@
 package juego;
 
 public class Acciones {
-    public static int ENVIAR_SELECCION          = 1; // Piedra papel o tijera
-    public static int ENVIAR_SELECCION_RIVAL    = 2;
-    public static int ENVIAR_NOMBRE             = 3;
-    public static int FINAL_DE_RONDA            = 4;
-    public static int FINAL_DE_ENFRENTAMIENTO   = 5;
-    public static int FINAL_DE_TORNEO           = 6;
+    public static final int ENVIAR_SELECCION          = 1; // Piedra papel o tijera
+    public static final int ENVIAR_SELECCION_RIVAL    = 2;
+    public static final int ENVIAR_NOMBRE             = 3;
+    public static final int FINAL_DE_RONDA            = 4;
+    public static final int FINAL_DE_ENFRENTAMIENTO   = 5;
+    public static final int FINAL_DE_TORNEO           = 6;
 
 
 
 
 
-    public static int PIEDRA                    = 100;
-    public static int PAPEL                     = 101;
-    public static int TIJERA                    = 102;
+    public static final int PIEDRA                    = 100;
+    public static final int PAPEL                     = 101;
+    public static final int TIJERA                    = 102;
 
-    public static int GANADOR_DE_RONDA          = 200;
-    public static int GANADOR_DE_ENFRENTAMIENTO = 201;
-    public static int GANADOR_DE_TORNEO         = 202;
+    public static final int GANADOR_DE_RONDA          = 200;
+    public static final int GANADOR_DE_ENFRENTAMIENTO = 201;
+    public static final int GANADOR_DE_TORNEO         = 202;
 
     public static final int PERDEDOR_DE_RONDA           = 203;
     public static final int PERDEDOR_DE_ENFRENTAMIENTO  = 204;
@@ -27,10 +27,10 @@ public class Acciones {
     public static final int EMPATE                      = 207;
 
 
-    public static int ERROR                     = -1;
-    public static int SELECCION_INCORRECTA      = -2;
+    public static final int ERROR                     = -1;
+    public static final int SELECCION_INCORRECTA      = -2;
 
-    public static int SEPARADOR                 = '|';
+    public static final int SEPARADOR                 = '|';
 
 }
 
