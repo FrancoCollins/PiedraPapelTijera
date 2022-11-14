6a4ac59883c8c9d107f85894b6a2e4b9140f4ac0
Franco <Francoernestocollins@gmail.com>
2022-11-14 12:37:24 +0100

    Se puede unir a partidas privadas con el codigo

--- src/juego/cliente/graphics/PantallaUnirseTorneo.java
+++ src/juego/cliente/graphics/PantallaUnirseTorneo.java
@@ -22,58 +22,56 @@ public class PantallaUnirseTorneo extends JPanel {
         titulo = new JLabel("Lista de torneos disponibles");
         titulo.setFont(new Font("TSCu_Comic", Font.BOLD, 34));
         titulo.setSize(500, 27);
-        titulo.setLocation(0,58);
+        titulo.setLocation(0, 58);
         setLayout(null);
         this.add(titulo);
 
 
         unirse_torneo = new JButton("Unirse al torneo");
-        unirse_torneo.setSize(144,27);
-        unirse_torneo.setLocation(150, 420);
+        unirse_torneo.setSize(144, 27);
+        unirse_torneo.setLocation(100, 420);
         this.add(unirse_torneo);
 
         entradaCodigoTorneo = new JTextField();
+        entradaCodigoTorneo.setSize(144, 27);
+        entradaCodigoTorneo.setLocation(250, 420);
+        this.add(entradaCodigoTorneo);
 
         this.setSize(500, 500);
         this.setVisible(true);
     }
 
-    public void onListaTorneoRecibida(JTable tabla){
+    public void onListaTorneoRecibida(JTable tabla) {
         this.tablaTorneos = tabla;
 
         JScrollPane panel_tabla = new JScrollPane(tabla);
-        panel_tabla.setSize(500,300);
-        panel_tabla.setLocation(0,100);
+        panel_tabla.setSize(500, 300);
+        panel_tabla.setLocation(0, 100);
         this.add(panel_tabla);
 
         unirse_torneo.addActionListener(e -> {
-            if(tabla.getRowCount() == 0)
-                return;
 
-            int columnaConLaClave = 2;
-            String clave = tabla.getValueAt(tabla.getSelectedRow(), columnaConLaClave).toString();
+            if (entradaCodigoTorneo.getText().length() != 0) {
 
-            graphics.getFunctionality().setClaveTorneo(clave);
-            graphics.getFunctionality().getSignalManager().enviarSenal(Senal.UNIRSE_TORNEO);
-            graphics.getFunctionality().getSignalManager().enviarPaquete(clave);
+                String clave = entradaCodigoTorneo.getText();
+
+                graphics.getFunctionality().setClaveTorneo(clave);
+                graphics.getFunctionality().getSignalManager().enviarSenal(Senal.UNIRSE_TORNEO);
+                graphics.getFunctionality().getSignalManager().enviarPaquete(clave);
+            } else {
+                if (tabla.getRowCount() == 0)
+                    return;
+
+                int columnaConLaClave = 2;
+                String clave = tabla.getValueAt(tabla.getSelectedRow(), columnaConLaClave).toString();
+
+                graphics.getFunctionality().setClaveTorneo(clave);
+                graphics.getFunctionality().getSignalManager().enviarSenal(Senal.UNIRSE_TORNEO);
+                graphics.getFunctionality().getSignalManager().enviarPaquete(clave);
+            }
+
+            revalidate();
+            repaint();
         });
-
-        // COMIENZO DE UNIRSE A TORNEO CON CODIGO Y BOTON DE REFRESCAR TABLA
-
-
-
-
-
-
-
-
-
-
-
-
-        // FINAL DE UNIRSE A TORNEO CON CODIGO Y BOTON DE REFRESCAR TABLA
-
-        revalidate();
-        repaint();
     }
 }
